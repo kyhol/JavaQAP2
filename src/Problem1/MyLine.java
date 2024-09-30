4dd59ada1334447defa9ed82fceb7055e9ca3c0d
kyhol <Kyle.Hollett@keyin.com>
2024-09-30 11:23:19 -0230

    Commit of MyLine, MyPoint and TesMyLine. Completed based on the diagram provided.

--- src/Problem1/MyLine.java
+++ src/Problem1/MyLine.java
@@ -5,4 +5,43 @@
 package Problem1;
 
 public class MyLine {
+    private MyPoint begin;
+    private MyPoint end;
+
+    // Constructors
+    public MyLine(int x1, int y1, int x2, int y2) {
+        begin = new MyPoint(x1, y1);
+        end = new MyPoint(x2, y2);
+    }
+
+    public MyLine(MyPoint begin, MyPoint end) {
+        this.begin = begin;
+        this.end = end;
+    }
+
+    // Getters and setters
+    public MyPoint getBegin() { return begin; }
+    public void setBegin(MyPoint begin) { this.begin = begin; }
+    public MyPoint getEnd() { return end; }
+    public void setEnd(MyPoint end) { this.end = end; }
+
+    // Implement other getters and setters as per the class diagram
+
+    // Calculate length
+    public double getLength() {
+        return begin.distance(end);
+    }
+
+    // Calculate gradient
+    public double getGradient() {
+        int xDiff = end.getX() - begin.getX();
+        int yDiff = end.getY() - begin.getY();
+        return Math.atan2(yDiff, xDiff);
+    }
+
+    // toString method
+    @Override
+    public String toString() {
+        return "MyLine[begins=" + begin + ", Ends=" + end + "]";
+    }
 }//end main
--- src/Problem1/TesMyLine.java
+++ src/Problem1/TesMyLine.java
@@ -5,4 +5,17 @@
 package Problem1;
 
 public class TesMyLine {
+    public static void main(String[] args) {
+        MyLine line1 = new MyLine(0, 0, 3, 4);
+        System.out.println("Line 1: " + line1);
+        System.out.println("Length: " + line1.getLength());
+        System.out.println("Gradient: " + line1.getGradient());
+
+        MyPoint p1 = new MyPoint(2, 2);
+        MyPoint p2 = new MyPoint(5, 6);
+        MyLine line2 = new MyLine(p1, p2);
+        System.out.println("Line 2: " + line2);
+
+        // Test other methods here
+    }
 }//end main
