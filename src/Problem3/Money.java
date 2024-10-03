36fbc2de12ec48ce9ad8a1a0a043bb5f8b2b4bf6
kyhol <Kyle.Hollett@keyin.com>
2024-10-03 10:45:44 -0230

    As is tradition, I forgot the copy constructor. It's added now

--- src/Problem3/Money.java
+++ src/Problem3/Money.java
@@ -1,7 +1,7 @@
 package Problem3;
 
 public class Money {
-    //define fields with long seperately because floating-point numbers will cause rounding errors
+    // Define fields with long separately because floating-point numbers will cause rounding errors
     private long dollars;
     private long cents;
 
@@ -10,27 +10,36 @@ public class Money {
         return dollars * 100 + cents;
     }
 
-    //copy Constructor define money, long as well.
+    // Constructor
     public Money(double amount) {
         this.dollars = (long) amount;
         this.cents = Math.round((amount - this.dollars) * 100.0);
     }
-    //add monies objects together and get new monies object
-    public Money add(Money otherAmount) {
-    long totalCents = this.getTotalCents() + otherAmount.getTotalCents();
-    return new Money(totalCents / 100.0);
-    }
-    //subtract monies objects from eachother creating new object
-    public Money subtract(Money otherAmount) {
-    long totalCents = this.getTotalCents() - otherAmount.getTotalCents();
-    return new Money(totalCents / 100.0);
-    }
-    //Compare total cents value
-    public int compareTo(Money otherAmount) {
-    return Long.compare(this.getTotalCents(), otherAmount.getTotalCents());
 
+    // Copy constructor
+    public Money(Money other) {
+        this.dollars = other.dollars;
+        this.cents = other.cents;
     }
-    @Override //check to see if objects are equal
+
+    // Add monies objects together and get new monies object
+    public Money add(Money otherAmount) {
+        long totalCents = this.getTotalCents() + otherAmount.getTotalCents();
+        return new Money(totalCents / 100.0);
+    }
+
+    // Subtract monies objects from each other creating new object
+    public Money subtract(Money otherAmount) {
+        long totalCents = this.getTotalCents() - otherAmount.getTotalCents();
+        return new Money(totalCents / 100.0);
+    }
+
+    // Compare total cents value
+    public int compareTo(Money otherAmount) {
+        return Long.compare(this.getTotalCents(), otherAmount.getTotalCents());
+    }
+
+    @Override // Check to see if objects are equal
     public boolean equals(Object obj) {
         if (this == obj)
             return true;
@@ -39,11 +48,10 @@ public class Money {
         Money other = (Money) obj;
         return this.getTotalCents() == other.getTotalCents();
     }
-    //convert to string. string representation = to format %d to display dollars in decimal and cents with %02d (to pad with 0's and 2 digits after the . )
+
+    // Convert to string. String representation = to format %d to display dollars in decimal and cents with %02d (to pad with 0's and 2 digits after the .)
     @Override
     public String toString() {
         return String.format("$%d.%02d", dollars, cents);
-
     }
-
 }
